0b274a48b4b242175609099069af249ef4bfafe8
tyrone890123 <gotyrone81@gmail.com>
2022-07-05 18:26:00 +0800

    fixed parts for grade and gpa

--- scripts/gpa.js
+++ scripts/gpa.js
@@ -64,26 +64,38 @@ function addrow() {
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
   var cell3 = row.insertCell(2);
+  row.insertCell(0).outerHTML = "<th scope='row' style='display: none;'></th>"
 
   //https://stackoverflow.com/questions/34091136/insert-a-new-row-of-input-tags-using-javascript
   var inputItem1 = document.createElement('input');
   inputItem1.setAttribute("type", "text");
   inputItem1.setAttribute("class", "form-control");
-  inputItem1.setAttribute("placeholder", "text");
+  inputItem1.setAttribute("placeholder", "Name of Course");
+  inputItem1.required = true;
   inputItem1.id = "name";
   cell1.appendChild(inputItem1);
   
   var inputItem2 = document.createElement('input');
   inputItem2.setAttribute("type", "number");
   inputItem2.setAttribute("class", "form-control");
-  inputItem2.setAttribute("placeholder", "number");
+  inputItem2.setAttribute("placeholder", "Units of Course");
+  inputItem2.setAttribute("min", "1");
+  inputItem2.setAttribute("max", "5");
+  inputItem2.setAttribute("oninvalid", "this.setCustomValidity('Please a number between 1 to 5.')");
+  inputItem2.setAttribute("oninput", "this.setCustomValidity('')");
+  inputItem2.required = true;
   inputItem2.id = "weight"
   cell2.appendChild(inputItem2);
 
   var inputItem3 = document.createElement('input');
   inputItem3.setAttribute("type", "number");
   inputItem3.setAttribute("class", "form-control");
-  inputItem3.setAttribute("placeholder", "number");
+  inputItem3.setAttribute("placeholder", "Grade Recieved");
+  inputItem3.setAttribute("min", "1");
+  inputItem3.setAttribute("max", "4");
+  inputItem3.setAttribute("oninvalid", "this.setCustomValidity('Please a number between 1 to 4.')");
+  inputItem3.setAttribute("oninput", "this.setCustomValidity('')");
+  inputItem3.required = true;
   inputItem3.id="percentage"
   cell3.appendChild(inputItem3);
 
@@ -102,13 +114,20 @@ function total(){
   var table = document.getElementById("table");
   let summation = 0;
   let divider = 0;
+  let checker =0;
   
   for(let i = 1; i<table.rows.length; i++){
+      if(Number(table.rows[i].cells[2].children[0].value)==0||Number(table.rows[i].cells[3].children[0].value)==0){
+        checker++;
+        break;
+      }
       let units=Number(table.rows[i].cells[2].children[0].value);
       let grade=Number(table.rows[i].cells[3].children[0].value);
       console.log(units*grade);
       summation+=units*grade;
       divider+=units;
   }
-  document.getElementById("result").value = (summation/divider).toFixed(2);
+  if(checker==0){
+    document.getElementById("result").value = (summation/divider).toFixed(2);
+  }
 }
--- scripts/grade.js
+++ scripts/grade.js
@@ -51,7 +51,7 @@ function loadSavedValues() {
         break;
       }
   }
-
+  
 }
 
 // insert other JS here
@@ -60,31 +60,44 @@ function addrow() {
   var table = document.getElementById("table");
   var row = table.insertRow(-1);
   row.id = "row"+(num).toString();
+
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
   var cell3 = row.insertCell(2);
   var cell4 = row.insertCell(3);
+  row.insertCell(0).outerHTML = "<th scope='row' style='display: none;'></th>"
 
   //https://stackoverflow.com/questions/34091136/insert-a-new-row-of-input-tags-using-javascript
   var inputItem1 = document.createElement('input');
   inputItem1.setAttribute("type", "text");
   inputItem1.setAttribute("class", "form-control");
-  inputItem1.setAttribute("placeholder", "text");
+  inputItem1.setAttribute("placeholder", "Name of Category");
   inputItem1.id = "name";
+  inputItem1.required = true;
   cell1.appendChild(inputItem1);
   
   var inputItem2 = document.createElement('input');
   inputItem2.setAttribute("type", "number");
   inputItem2.setAttribute("class", "form-control");
-  inputItem2.setAttribute("placeholder", "number");
+  inputItem2.setAttribute("placeholder", "Weighted of Category");
+  inputItem2.setAttribute("min", "1");
+  inputItem2.setAttribute("max", "100");
+  inputItem2.setAttribute("oninvalid", "this.setCustomValidity('Please a number between 1 to 100.')");
+  inputItem2.setAttribute("oninput", "this.setCustomValidity('')");
   inputItem2.id = "weight"
+  inputItem2.required = true;
   cell2.appendChild(inputItem2);
 
   var inputItem3 = document.createElement('input');
   inputItem3.setAttribute("type", "number");
   inputItem3.setAttribute("class", "form-control");
-  inputItem3.setAttribute("placeholder", "number");
+  inputItem3.setAttribute("placeholder", "Your Grade Percent");
+  inputItem3.setAttribute("min", "1");
+  inputItem3.setAttribute("max", "100");
+  inputItem3.setAttribute("oninvalid", "this.setCustomValidity('Please a number between 1 to 100.')");
+  inputItem3.setAttribute("oninput", "this.setCustomValidity('')");
   inputItem3.id="percentage"
+  inputItem3.required = true;
   cell3.appendChild(inputItem3);
 
   cell4.innerHTML = "-";
@@ -103,13 +116,21 @@ function delrow() {
 function total(){
   var table = document.getElementById("table");
   let totaln = 0;
-  
+  let checker =0;
+
   for(let i = 1; i<table.rows.length; i++){
-      let weight=Number(table.rows[i].cells[2].children[0].value);
-      let percent=Number(table.rows[i].cells[3].children[0].value);
-      document.getElementById("table").rows[i].cells[4].innerHTML = weight*(percent*0.01);
-      totaln+=weight*(percent*0.01);
-      
+    if(Number(table.rows[i].cells[2].children[0].value)==0||Number(table.rows[i].cells[3].children[0].value)==0){
+      checker++;
+      break;
+    }
+    let weight=Number(table.rows[i].cells[2].children[0].value);
+    let percent=Number(table.rows[i].cells[3].children[0].value);
+    console.log(weight);
+    table.rows[i].cells[4].innerHTML = weight*(percent*0.01);
+    totaln+=weight*(percent*0.01);
   }
-  document.getElementById("result").value = totaln;
+  if(checker==0){
+    document.getElementById("result").value = totaln;
+  }
+  
 }
